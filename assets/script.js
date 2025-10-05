ce93b35c30d34c788c3a339ef89825daf579931b
zaitken <zaraaitken18@gmail.com>
2025-10-05 14:20:26 +0100

    adds container class to navigtion to standardise margins

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU ON SMALLER SCREENS
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const contactBtn = document.querySelector('.nav-btn');
